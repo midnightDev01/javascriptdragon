dca03506a793ba1ed7c25d5ce55a2035f56d516a
Raissa Rodrigues Penha <raissa.rodrigues.penha@myrasecurity.com>
2021-10-30 17:01:13 +0200

    Use injector

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var dragonApp = angular.module('dragonApp', []);
+const dragonApp = angular.module('dragonApp', []);
 
 dragonApp.controller('mainController', ['$scope', function ($scope) {
-    
+    console.log($scope);
 }]);
 
+const createDragon = function(size, wings, type){
+    return 'Scaly lizard';
+}
 
-
+console.log(angular.injector().annotate(createDragon));
