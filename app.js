73be512ec70c0081e79aeb7b55c01c3bda652e9d
Raissa Rodrigues Penha <raissa.rodrigues.penha@myrasecurity.com>
2021-11-01 22:20:15 +0100

    Dragon

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const dragonApp = angular.module('dragonApp', []);
 
-dragonApp.controller('mainController', ['$scope', function ($scope) {
-    console.log($scope);
+dragonApp.controller('mainController', ['$scope', '$log', '$filter', ($scope, $log, $filter) => {
+    $log.log("Log service");
+
+    $scope.dragon = 'Varanus komodoensis';
+    $scope.formattedString = $filter('uppercase')($scope.dragon);
+
+    $log.info($scope.dragon);
+    $log.info($scope.formattedString);
+
 }]);
 
 const createDragon = function(size, wings, type){
